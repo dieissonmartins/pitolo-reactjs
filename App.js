5bae14d682e9ba154d42bd5203752431f6ce1d74
Dieisson <dieisson@mundowap.com.br>
2025-10-08 10:09:02 -0300

    quebra em componentes

--- App.js
+++ App.js
@@ -2,6 +2,21 @@ import React, {Component} from 'react';
 
 class App extends Component {
     render() {
+        return (
+            <div>
+                <Title/>
+                <SubTitle/>
+                <Description/>
+                <Sum/>
+                <Bool/>
+            </div>
+        );
+    }
+}
+
+class Title extends Component {
+
+    render() {
 
         let titleStyle = {
             fontSize: '2em',
@@ -9,17 +24,51 @@ class App extends Component {
             color: '#003300'
         }
 
+        return (
+            <h1 style={titleStyle}>Título</h1>
+        );
+    }
+}
+
+class SubTitle extends Component {
+    render() {
+        return (
+            <h2>Subtítulo</h2>
+        );
+    }
+}
+
+class Description extends Component {
+    render() {
+        return (
+            <p data-description={'demo'}>Descrição</p>
+        );
+    }
+}
+
+class Sum extends Component {
+    render() {
+        return (
+            <span className={'hello'}>{25 + 20}</span>
+        );
+    }
+}
+
+class Comment extends Component {
+    render() {
+        return (
+            {/* This is a comment in JSX */}
+        );
+    }
+}
+
+class Bool extends Component {
+    render() {
+
         let i = 5;
 
         return (
-            <div>
-                <h1 style={titleStyle}>Título</h1>
-                <h2>Subtítulo</h2>
-                <p data-description={'demo'}>Descrição</p>
-                <span className={'hello'}>{25 + 20}</span>
-                {/* This is a comment in JSX */}
-                <span>{i === 1 ? "True!" : "False!"}</span>
-            </div>
+            <span>{i === 1 ? "True!" : "False!"}</span>
         );
     }
 }
