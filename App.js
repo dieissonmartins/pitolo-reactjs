e8db7d6bec3d6cd1289988335bb6ab49c6e91eba
Dieisson <dieisson@mundowap.com.br>
2025-10-08 09:53:03 -0300

    add propiedades de style

--- App.js
+++ App.js
@@ -2,9 +2,20 @@ import React, {Component} from 'react';
 
 class App extends Component {
     render() {
+
+        var titleSyle = {
+            fontSize: '2em',
+            fontFamily: 'Courier',
+            color: '#003300'
+        }
+
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1 style={titleSyle}>Título</h1>
+                <h2>Subtítulo</h2>
+                <p data-description={'demo'}>Descrição</p>
+                <span className={'hello'}>{25 + 20}</span>
+                {/* This is a comment in JSX */}
             </div>
         );
     }
