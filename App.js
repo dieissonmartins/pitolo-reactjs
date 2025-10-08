654c2918e98bfbc3631ec5668899ebaab9c2d701
Dieisson <dieisson@mundowap.com.br>
2025-10-08 09:57:02 -0300

    teste ternario

--- App.js
+++ App.js
@@ -3,19 +3,22 @@ import React, {Component} from 'react';
 class App extends Component {
     render() {
 
-        var titleSyle = {
+        let titleStyle = {
             fontSize: '2em',
             fontFamily: 'Courier',
             color: '#003300'
         }
 
+        let i = 5;
+
         return (
             <div>
-                <h1 style={titleSyle}>Título</h1>
+                <h1 style={titleStyle}>Título</h1>
                 <h2>Subtítulo</h2>
                 <p data-description={'demo'}>Descrição</p>
                 <span className={'hello'}>{25 + 20}</span>
                 {/* This is a comment in JSX */}
+                <span>{i === 1 ? "True!" : "False!"}</span>
             </div>
         );
     }
