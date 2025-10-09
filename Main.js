dbf1ccc5305a72b74d38e710bbcdb09c4d24917f
Dieisson <dieisson@mundowap.com.br>
2025-10-09 14:11:44 -0300

    roteamento com reactjs

--- Main.js
+++ Main.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import About from './About';
+import Contact from './Contact';
+
+const routing = (
+    <Router>
+        <div>
+            <h1>Projeto Rotas</h1>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </div>
+    </Router>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
-
-root.render(<App name={'Dieisson'} />);
+root.render(routing);
