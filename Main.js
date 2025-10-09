b7a6d32c0859e0e45feff0eb217396b2fbe32fe7
Dieisson <dieisson@mundowap.com.br>
2025-10-09 14:16:19 -0300

    navegacao com rotas

--- Main.js
+++ Main.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Link, Routes, Route, NavLink} from 'react-router-dom';
 import App from './App';
 import About from './About';
 import Contact from './Contact';
+import Notfound from "./notfound";
 
 const routing = (
     <Router>
         <div>
             <h1>Projeto Rotas</h1>
+            <ul>
+                <li>
+                    <NavLink to="/" exact activeStyle={{color: 'red'}}>
+                        Painel
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" exact activeStyle={{color: 'red'}}>
+                        Sobre
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" exact activeStyle={{color: 'red'}}>
+                        Contato
+                    </NavLink>
+                </li>
+            </ul>
             <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/" element={<App/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route component={Notfound} />
             </Routes>
         </div>
     </Router>
